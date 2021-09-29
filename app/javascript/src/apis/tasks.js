03f8a2e040f5018f8664406b02b6ddfa9c973c0e
vijay2136 <imvijay2136@gmail.com>
2021-09-29 19:59:19 +0530

    Added ability to delete a task

--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -4,12 +4,14 @@ const list = () => axios.get("/tasks");
 const create = payload => axios.post("/tasks/", payload);
 const show = slug => axios.get(`/tasks/${slug}`);
 const update = ({ slug, payload }) => axios.put(`/tasks/${slug}`, payload);
+const destroy = slug => axios.delete(`/tasks/${slug}`);
 
 const tasksApi = {
   list,
   create,
   show,
-  update
+  update,
+  destroy
 };
 
 export default tasksApi;

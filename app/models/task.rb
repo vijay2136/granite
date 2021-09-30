2aba5ccf586c4f60589b8e164997f2f77a5d6494
vijay2136 <imvijay2136@gmail.com>
2021-09-30 13:20:28 +0530

    Added ability to assign task to a user

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { maximum: 50 }
   validates :slug, uniqueness: true
   validate :slug_not_changed
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
 
   resources :tasks, except: %i[new edit], param: :slug
+  resources :users, only: :index
   root "home#index"
   get "*path", to: "home#index", via: :all
 end
